7bd15b15ce0ab39ad8a993e02b2ebc7870823ec7
Daniel Madrid <madrid2797@hotmail.com>
2018-02-26 13:44:24 -0800

    Add files via upload

---
+++ 180221_actividad/180221_actividad/CustomInputStream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180221_actividad
+{
+  public  class LowerInputStream : System.IO.StreamReader
+    {
+        public LowerInputStream(Stream stream) : base(stream) { }
+        public override int Read()
+        {
+            int c = base.Read();
+            if (c == 1)
+            {
+                return -1;
+            }
+            else
+            {
+                return char.ToLower((char)c);
+            }
+        }
+    }
+}
---
+++ 180221_actividad/180221_actividad/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _180221_actividad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //string path = @"C:Users\MADRI\source\repos\180221_actividad\180221_actividad\bin\Ejemplo.txt";
+            string path = "NoImporta.txt";
+            try
+            {
+                if (File.Exists(path))
+                {
+                   //  File.Delete(path);
+                  
+                }
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("HOLA SOY UNOS CARACTERS EN MINUSCULA");
+               
+                }
+
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (LowerInputStream sr = new LowerInputStream(fs))
+                    {
+
+                        while (sr.Peek() >= 0)
+                        {
+                            Console.WriteLine(Convert.ToChar(sr.Read()) );
+                           
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
+
